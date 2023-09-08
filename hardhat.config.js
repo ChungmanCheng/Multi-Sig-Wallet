4160a14e9b4eace54c66552d618f565cdc55ff3f
erwin <erwinchengchungman916@gmail.com>
2023-09-08 18:19:23 +0800

    Wallet now supports ERC20 transactions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ganache");
+require("@nomiclabs/hardhat-ethers");
 require("solidity-coverage");
 require("hardhat-deploy");
 
 require("dotenv").config();
 
-const Goerli_RPC_URL = process.env.Goerli_RPC_URL;
-const Mumbai_RPC_URL = process.env.Mumbai_RPC_URL;
 const Sepolia_RPC_URL = process.env.Sepolia_RPC_URL;
-const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
